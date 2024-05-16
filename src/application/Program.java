9e18e3b0e3d391e04c0b9b40dfede3c0a7856682
Lucas Rocha <lucasrocha478@gmail.com>
2024-05-16 10:54:39 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,11 +26,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TEST 2 - FIND ALL ===");
+		System.out.println("=== TEST 3 - FIND ALL ===");
 		List<Seller> list2 = sellerDao.findAll();
 		for(Seller obj : list2) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== TEST 4 - INSERT ===");
+		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.00, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
