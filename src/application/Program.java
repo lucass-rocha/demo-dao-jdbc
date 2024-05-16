89034048e3c7b06cbbc4232f70e715f4d98c48f3
Lucas Rocha <lucasrocha478@gmail.com>
2024-05-16 09:36:37 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 
@@ -9,11 +12,18 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		System.out.println("=== TEST 1 - FIND BY ID ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("=== TEST 2 - FIND BY DEPARTMENT ===");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -18,12 +18,13 @@ public class Seller implements Serializable{
 	public Seller() {
 	}
 
-	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
+		this.department = department;
 	}
 
 	public Integer getId() {
@@ -108,7 +109,7 @@ public class Seller implements Serializable{
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + "]";
+				+ baseSalary +  ", department=" + department +"]";
 	}
 	
 	
