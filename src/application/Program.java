a1ace2f0c515f84e9356e9e917739974cbdf89eb
Lucas Rocha <lucasrocha478@gmail.com>
2024-06-04 10:59:06 -0300

    Delete by ID Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,6 +14,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST 1 - FIND BY ID ===");
@@ -42,6 +44,15 @@ public class Program {
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update Complete");
+		
+		System.out.println("=== TEST 6 - SELLER DELETE BY ID ===");
+		System.out.println("Enter ID for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
+
 		
 
 	}
